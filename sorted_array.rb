e44536ff02e5f8f1eb6bde5e5670fe73ef6ab4fe
melodicodyssey84 <stevedbauer@gmail.com>
2014-01-14 21:16:55 -0800

    Tuesday night code 1-14-2014

--- sorted_array.rb
+++ sorted_array.rb
@@ -1,6 +1,10 @@
 class SortedArray
   attr_reader :internal_arr
 
+  def internal_arr
+      return @internal_arr
+    end
+
   def initialize arr=[]
     @internal_arr = []
     arr.each { |el| add el }
@@ -38,22 +42,127 @@ class SortedArray
   end
 
   def each &block
-    raise NotImplementedError.new("You need to implement the each method!")
+    # raise NotImplementedError.new("You need to implement the each method!")
+    i = 0
+    while i < @internal_arr.length
+      yield @internal_arr[i]
+      i += 1
+    end
+    @internal_arr
   end
 
   def map &block
-    raise NotImplementedError.new("You need to implement the map method!")
+    # raise NotImplementedError.new("You need to implement the map method!")
+    new_array = []
+    each {|element| new_array << (yield element) }
+    new_array
+
+    # new_array = []
+    # i = 0
+    # while i < @internal_arr.length
+    #   new_array << (yield @internal_arr[i])
+    #   i += 1
+    # end
+    # new_array
   end
 
   def map! &block
-    raise NotImplementedError.new("You need to implement the map! method!")
+    # raise NotImplementedError.new("You need to implement the map! method!")
+  
+    new_array = []
+    each {|element| new_array << (yield element)}
+    @internal_arr = new_array
+
+
+  # new_array
+  # i = 0
+  # while i < @internal_arr.length
+  #   new_array << (yield @internal_arr[i])
+  #   i+=1
+  # end
+  # @internal_arr = new_array
+  # @internal_arr
   end
 
-  def find value
-    raise NotImplementedError.new("You need to implement the find method!")
+  def find &value
+    # raise NotImplementedError.new("You need to implement the find method!")
+      # i = 0
+      # while i < @internal_arr.length
+      #   if @internal_arr[i] == (yield value)
+      #     return value
+      #   end
+      #   i += 1
+      # end
+      # return 
+      each do |element| 
+        if yield element
+          return element
+        end
+      end
+      return nil
   end
 
   def inject acc=nil, &block
-    raise NotImplementedError.new("You need to implement the inject method!")
+    # raise NotImplementedError.new("You need to implement the inject method!")
+    # inject takes and argument and a block
+    # inject(arg=0){block to run on each element}
+
+
+    # run block on each element of @internal_arr, update acc
+    # each do |element|
+    #   yield acc
+    #   acc = yield element
+    # end
+
+    if acc.class == Symbol
+      method = acc.to_proc
+      acc = 0
+
+      each do |element|
+        acc = method(acc,element)
+      end
+
+
+    # # elsif block = nil && acc != nil
+    # #   method = acc.to_sym.to_proc
+    # #   acc = 0
+    # #   each do |element|
+    # #     acc = method(element)
+    # #   end
+      return acc
+    end
+
+    acc == nil ? acc = 0 : acc = acc
+    
+    each do |element|
+      # block.call(acc, element)
+      acc = (yield acc, element)
+    end
+
+    return acc
+
+
   end
 end
+
+# ex1 = SortedArray.new([1,2,3,4,5])
+# puts ex1.inject(:+)
+
+# [1,2,3,4,5]   
+
+#   acc = 0
+# each do |sum,element| sum + element
+#   acc = {|0,1| 0 + 1} => 1
+#   acc = {|1,2| 1 + 2} => 3
+#   acc = {|3,3| 3 + 3} => 6
+#   acc = {|6,4| 6 + 4} => 10
+#   acc = {|10,5| 10 + 5} => 15
+
+#   acc = 15
+#   return acc
+
+=begin
+  
+  each do (:+)
+  
+=end
--- sorted_array_spec.rb
+++ sorted_array_spec.rb
@@ -34,7 +34,10 @@ describe SortedArray do
         it_should_behave_like "yield to all elements in sorted array", :map
 
         it 'creates a new array containing the values returned by the block' do
-          pending "fill this spec in with a meaningful example"
+          # pending "fill this spec in with a meaningful example"
+          original_array = sorted_array.internal_arr
+
+
         end
       end
     end
@@ -42,31 +45,48 @@ describe SortedArray do
     describe "that update the original array" do
       describe :map! do
         it 'the original array should be updated' do
-          pending "fill this spec in with a meaningful example"
+          # pending "fill this spec in with a meaningful example"
+          # original_array = sorted_array.internal_arr
+          # sorted_array.map {|el| el}
+          # sorted_array.should != original_array
+          expect {sorted_array.map!{|x| x*2}}.to change{sorted_array.internal_arr}.from([2,3,4,7,9]).to([4,6,8,14,18])
         end
 
         it_should_behave_like "yield to all elements in sorted array", :map!
 
         it 'should replace value of each element with the value returned by block' do
-          pending "this is just the same as the example above"
+          # pending "this is just the same as the example above"
+          sorted_array.map {|x| x*2}.should == [4,6,8,14,18]
         end
       end
     end
   end
 
   describe :find do
-    it_should_behave_like "yield to all elements in sorted array", :find
+    #change below description to: stop after finding element
+    # it_should_behave_like "yield to all elements in sorted array", :find
 
-    it "does not currently have any examples for it" do
-      pending "define some examples by looking up http://www.ruby-doc.org/core-2.1.0/Enumerable.html#method-i-find"
+    it "should return nil if no matches exist" do
+      # pending "define some examples by looking up http://www.ruby-doc.org/core-2.1.0/Enumerable.html#method-i-find"
+      sorted_array.find{|x| x == 100}.should == nil
     end
+
+    it "should return first value that evaluates to true" do
+    sorted_array.find{|x| x == 3}.should == 3
+  end
   end
 
   describe :inject do
-    it_should_behave_like "yield to all elements in sorted array", :inject
+    # it_should_behave_like "yield to all elements in sorted array", :inject
 
-    it "does not currently have any examples for it" do
-      pending "define some examples by looking up http://www.ruby-doc.org/core-2.1.0/Enumerable.html#method-i-inject"
+    it "iterates through an array while updating an accumulator value" do
+      # pending "define some examples by looking up http://www.ruby-doc.org/core-2.1.0/Enumerable.html#method-i-inject"
+      sorted_array.inject(0){|sum,el| sum + el}.should == 25
     end
+
+    it "matches the .inject kernel" do
+      sorted_array.inject(:+).should == source.inject(:+)
+    end
+
   end
 end
